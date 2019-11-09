598718b3a33d3dbb6c7c12d99e785597b9175fbc
Bruno Arthur <brunoartc@yahoo.com>
2019-11-09 16:13:10 -0300

    fix script
    install openvpn on init

--- ec2.sh
+++ ec2.sh
@@ -1,7 +1,7 @@
 sudo apt -y update
-sudo apt install npm
+sudo apt install npm openvpn
 cd /home/ubuntu
 git clone https://github.com/brunoartc/cloud_database
 cd cloud_database
 npm i 
-npm start
+tmux new -d -s todo 'npm start;' 
